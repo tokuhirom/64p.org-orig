b8dd376ce3340f0683a8af3db7a3a89d901c01d1
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-06-14 05:51:08 +0900

    updated screen shots

--- script/maintenance/mk_screenshots.pl
+++ script/maintenance/mk_screenshots.pl
@@ -5,14 +5,13 @@ use utf8;
 use 5.010000;
 use Digest::MD5 qw/md5_hex/;
 use autodie;
-use Log::Minimal;
 
 open my $fh, '<:utf8', 'tmpl/index.tt';
 while (<$fh>) {
-    /screen_shot\(['"](.+)['"]\)/ and do {
+    /url\s*=>\s*['"](.+)['"]/ and do {
         my $url = $1;
-        infof("processing '%s'", $url);
-        system "webkit2png", '-C', "--dir=htdocs/static/img/screen_shot/", "--md5", $url;
+        printf("processing '%s'\n", $url);
+        system "webkit2png", '-C', "--dir=static/img/screen_shot/", "--md5", $url;
     };
 }
 
